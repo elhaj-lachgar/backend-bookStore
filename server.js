d805a87f1341da78f4e0764b1023644db9199315
elhaj-lachgar <143610909+elhaj-lachgar@users.noreply.github.com>
2024-01-11 17:02:25 +0100

    handle10

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const ConnectDB = require('./config/ConnetedDB');
 const AddressRoute = require('./api/AddressRoute');
 const PaymentRoute = require('./api/PayementRoute');
 const { webHookService } = require("./services/PayementService");
-const ParseRequiset = require("./middleware/ParseReq");
+
 
 
 
@@ -36,7 +36,7 @@ app.options("*" ,cors());
 app.use(morgan("dev"))
 
 
-app.post("/api/v1/webhook" ,express.raw({type : 'application/json'}), webHookService);
+app.post("/api/v1/webhook" ,express.raw({type : 'application/json'}), webHookService);o
 
 app.use(express.json())
 
